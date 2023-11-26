529645d3128bf9cc9dce115b9e2c62ae0ea12d5c
MasonBoom <masonboom@gmail.com>
2023-11-25 21:14:53 -0500

    Updated GPT

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 require("dotenv").config();
 const inquirer = require("inquirer");
+const { PromptTemplate } = require("langchain/prompts");
 const { OpenAI } = require("langchain/llms/openai");
+const { StructuredOutputParser } = require("langchain/output_parsers");
 
 const model = new OpenAI({
   openAIApiKey: process.env.OPENAI_API_KEY,
   temperature: 0,
-  model: "gpt-3.5-turbo",
+  model: "gpt-4",
 });
+
+const parser = StructuredOutputParser.fromNamesAndDescriptions({
+  code: "Javascript code that answers the user's question",
+  explanation: "detailed explanation of the example code provided",
+});
+
+const formatInstructions = parser.getFormatInstructions();
 
 // Uses the instantiated OpenAI wrapper, model, and makes a call based on input from inquirer
 const promptFunc = async (input) => {
   try {
+    const prompt = new PromptTemplate({
+      template: "You are a javascript expert and will answer the user’s coding questions thoroughly as possible.\n{format_instructions}\n{question}",
+      inputVariables: ["question"],
+      partialVariables: { format_instructions: formatInstructions }
+    });
+    const promptInput = await prompt.format({
+      question: input
+    });
     const res = await model.call(input);
-    console.log(res);
+    console.log(await parser.parse(res));
   } catch (err) {
     console.error(err);
   }
